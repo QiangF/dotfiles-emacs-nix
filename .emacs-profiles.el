f428720653e6fc8956d51024ec1e1ed23428ca92
Ben Whitley <dev@purplg.com>
2021-08-31 15:34:20 -0400

    empty profile

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,2 +1,3 @@
 (("purplg" . ((user-emacs-directory . "~/.emacs.profiles.d/purplg")))
+ ("empty" . ((user-emacs-directory . "~/.emacs.profiles.d/empty")))
  ("doom" . ((user-emacs-directory . "~/.emacs.profiles.d/doom"))))
