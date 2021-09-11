495eb8c343ff2520d60cdbf5e98b4ebc6c7417a0
Ben Whitley <dev@purplg.com>
2021-09-11 19:00:43 -0400

    fixed straight bootstrap

--- .emacs.profiles.d/purplg/init.el
+++ .emacs.profiles.d/purplg/init.el
@@ -6,16 +6,16 @@
 ;; Straight.el bootstrap for package management
 (defvar bootstrap-version)
 (let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory)))
-    (bootstrap-version 5)
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-      (url-retrieve-synchronously
-        "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-        'silent 'inhibit-cookies)
-      (goto-char (point-max)))
-    (eval-print-last-sexp)))
-(load bootstrap-file nil 'nomessage)
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
 (straight-use-package 'use-package)
 (setq straight-disable-native-compile nil)
