3fd19524a0b2643d451912ee4d28463af6baa50c
Ben Whitley <dev@purplg.com>
2021-10-03 13:56:53 -0400

    added back missing tweaks

--- .emacs.profiles.d/purplg/init.el
+++ .emacs.profiles.d/purplg/init.el
@@ -6,6 +6,10 @@
 (defconst pg/module-dir (expand-file-name "modules/" pg/config-dir))
 (add-to-list 'load-path pg/module-dir)
 
+(setq native-comp-async-report-warnings-errors nil)
+(setq ring-bell-function 'ignore)
+(setq scroll-conservatively 101)
+
 (require 'pg-basics)
 (require 'pg-evil)
 (require 'pg-interface)
