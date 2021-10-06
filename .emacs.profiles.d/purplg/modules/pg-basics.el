2ee18f656592494d51420807b72c2d583ca0c814
Ben Whitley <dev@purplg.com>
2021-10-06 14:33:25 -0400

    make emacs-lisp-mode default scratch buffer mode

--- .emacs.profiles.d/purplg/modules/pg-basics.el
+++ .emacs.profiles.d/purplg/modules/pg-basics.el
@@ -4,6 +4,7 @@
 
 (setq native-comp-async-report-warnings-errors nil
       ring-bell-function 'ignore
-      scroll-conservatively 101)
+      scroll-conservatively 101
+      initial-major-mode 'emacs-lisp-mode)
 
 (provide 'pg-basics)
