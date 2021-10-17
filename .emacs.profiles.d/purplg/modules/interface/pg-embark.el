2eb62f79165627b1fa7672fd66100137f5914d10
Ben Whitley <dev@purplg.com>
2021-10-17 11:06:19 -0400

    proper embark keymaps

--- .emacs.profiles.d/purplg/modules/interface/pg-embark.el
+++ .emacs.profiles.d/purplg/modules/interface/pg-embark.el
@@ -7,6 +7,10 @@
   :config
   (general-define-key
    :states '(normal visual insert)
-   "C-." #'embark-act))
+   "C-." #'embark-act)
+  (general-define-key
+   :keymap 'minibuffer-local-map
+   "C-." #'embark-act
+   "C-," #'embark-become))
 
 (provide 'pg-embark)
