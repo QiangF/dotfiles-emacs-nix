64a99b3bc1eaa0670489c0a00b7c2af62b881c15
Ben Whitley <dev@purplg.com>
2021-10-17 11:06:36 -0400

    rss keymap

--- .emacs.profiles.d/purplg/modules/apps/pg-rss.el
+++ .emacs.profiles.d/purplg/modules/apps/pg-rss.el
@@ -6,6 +6,9 @@
   :config
   (setq elfeed-feeds
         '(("https://planet.emacslife.com/atom.xml")
-          ("https://protesilaos.com/codelog.xml"))))
+          ("https://protesilaos.com/codelog.xml")))
+  (pg/leader
+   :states 'normal
+   "o r" #'(elfeed :wk "rss")))
 
 (provide 'pg-rss)
