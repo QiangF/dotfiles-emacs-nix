68a5f0f3934f8fe886bddd75f4e8ea969c39552a
Ben Whitley <dev@purplg.com>
2021-10-23 09:31:19 -0400

    Hass usage for testing

--- .emacs.profiles.d/purplg/modules/system-specific/pg-desktop.el
+++ .emacs.profiles.d/purplg/modules/system-specific/pg-desktop.el
@@ -9,12 +9,29 @@
 ;; Dev
 (use-package hass
   :straight '(:local-repo "~/code/elisp/hass")
-  :init (use-package request :straight t)
-        (use-package websocket :straight t)
-        (setq hass-host "homeassistant")
-        (setq hass-insecure t)
-        (setq hass-apikey (auth-source-pass-get 'secret "home/hass/emacs-apikey"))
-        (hass-setup))
+
+  :init
+  ;; Dev packages aren't resolved automatically
+  (use-package request :straight t)
+  (use-package websocket :straight t)
+
+  :config
+  ;; Initial config
+  (setq hass-host "homeassistant")
+  (setq hass-insecure t)
+  (setq hass-apikey (auth-source-pass-get 'secret "home/hass/emacs-apikey"))
+  (add-to-list 'hass-watched-entities "switch.bedroom_light")
+  (hass-setup)
+
+  ;; An automation just to "eat my own dogfood".
+  ;; Changes Emacs theme based on the state of my bedroom light.
+  (add-hook 'hass-entity-state-updated-functions
+    (lambda (entity-id)
+      (cond ((string= entity-id "switch.bedroom_light")
+             (if (hass-switch-p entity-id)
+                 (load-theme 'doom-one-light t)
+                 (load-theme 'doom-dracula t))))))
+  (hass-realtime-mode t))
 
 ;; Prod
 (use-package hass
