0037d2245bf8ecdc376ff9af0dc88bfa11119a33
Ben Whitley <dev@purplg.com>
2021-10-24 16:37:37 -0400

    theme setting/tracking functions

--- .emacs.profiles.d/purplg/modules/interface/pg-theme.el
+++ .emacs.profiles.d/purplg/modules/interface/pg-theme.el
@@ -1,12 +1,21 @@
 ;;; --- -*- lexical-binding: t; -*-
 (require 'pg-straight)
 
+(defvar current-theme nil)
+
+(defun set-theme (theme-name)
+  (setq current-theme theme-name)
+  (load-theme current-theme t))
+
+;; Default theme
+(set-theme 'wombat)
+
 (use-package doom-themes
   :straight t
   :config
   (setq doom-themes-enable-bold t)
   (setq doom-themes-enable-italic t)
 
-  (load-theme 'doom-dracula t))
+  (set-theme 'doom-dracula))
 
 (provide 'pg-theme)
