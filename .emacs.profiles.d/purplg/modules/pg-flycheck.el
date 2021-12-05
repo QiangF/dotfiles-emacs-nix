abf703b5f55964fc8c0bbec765d71cc7f35d8642
Ben Whitley <dev@purplg.com>
2021-12-05 10:03:37 -0500

    next/prev error keybinds

--- .emacs.profiles.d/purplg/modules/pg-flycheck.el
+++ .emacs.profiles.d/purplg/modules/pg-flycheck.el
@@ -3,6 +3,11 @@
 
 (use-package flycheck
   :config
-  (hook! 'lsp-mode-hook #'flycheck-mode))
+  (hook! 'lsp-mode-hook #'flycheck-mode)
+  (general-define-key
+   :states 'normal
+   "M-h" #'(flycheck-previous-error :wk "prev error")
+   "M-l" #'(flycheck-next-error :wk "next error")))
+ 
 
 (provide 'pg-flycheck)
