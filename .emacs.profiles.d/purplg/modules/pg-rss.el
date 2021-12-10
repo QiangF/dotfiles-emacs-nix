92e01c0365704c55461f4056541ac4a3beb23557
Ben Whitley <dev@purplg.com>
2021-12-10 17:20:27 -0500

    add gw2 rss

--- .emacs.profiles.d/purplg/modules/pg-rss.el
+++ .emacs.profiles.d/purplg/modules/pg-rss.el
@@ -4,7 +4,8 @@
   :config
   (setq elfeed-feeds
         '(("https://planet.emacslife.com/atom.xml")
-          ("https://protesilaos.com/codelog.xml")))
+          ("https://protesilaos.com/codelog.xml")
+          ("https://www.guildwars2.com/en/feed/")))
   (pg/leader
    :states 'normal
    "o r" #'(elfeed :wk "rss")))
