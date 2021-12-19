f27a6b7902bef27560a5c3e82a603a12649455e1
Ben Whitley <dev@purplg.com>
2021-12-19 10:07:39 -0500

    fix spacing

--- .emacs.profiles.d/purplg/modules/pg-popper.el
+++ .emacs.profiles.d/purplg/modules/pg-popper.el
@@ -1,20 +1,20 @@
 ;;; --- -*- lexical-binding: t; -*-
 
 (use-package popper
- :general
- ("C-`" #'popper-toggle-latest)
- ("M-`" #'popper-cycle)
- ("C-M-`" #'popper-toggle-type)
- :init
- (setq popper-reference-buffers '("\\*Messages\\*"
-                                  "^\\*helpful.*\\*$"
-                                  "^\\*eldoc.*\\*$"
-                                  flycheck-error-list-mode
-                                  compilation-mode
-                                  rustic-compilation-mode
-                                  flymake-diagnostics-buffer-mode
-                                  flymake-project-diagnostics-mode))
- :config
- (popper-mode +1))
+  :general
+  ("C-`" #'popper-toggle-latest)
+  ("M-`" #'popper-cycle)
+  ("C-M-`" #'popper-toggle-type)
+  :init
+  (setq popper-reference-buffers '("\\*Messages\\*"
+                                    "^\\*helpful.*\\*$"
+                                    "^\\*eldoc.*\\*$"
+                                    flycheck-error-list-mode
+                                    compilation-mode
+                                    rustic-compilation-mode
+                                    flymake-diagnostics-buffer-mode
+                                    flymake-project-diagnostics-mode))
+  :config
+  (popper-mode +1))
   
 (provide 'pg-popper)
