48f16d6232a97e48adefcf4483157b031135484a
Ben Whitley <dev@purplg.com>
2021-12-19 10:08:23 -0500

    disable flycheck in favor of flymake

--- .emacs.profiles.d/purplg/modules/pg-flycheck.el
+++ .emacs.profiles.d/purplg/modules/pg-flycheck.el
@@ -2,6 +2,7 @@
 (require 'pg-lsp)
 
 (use-package flycheck
+  :disabled
   :config
   (hook! 'lsp-mode-hook #'flycheck-mode)
   (general-define-key
@@ -9,5 +10,11 @@
    "M-h" #'(flycheck-previous-error :wk "prev error")
    "M-l" #'(flycheck-next-error :wk "next error")))
  
+(use-package flymake
+  :config
+  (general-define-key
+   :states 'normal
+   "M-k" #'(flymake-goto-prev-error :wk "prev error")
+   "M-j" #'(flymake-goto-next-error :wk "next error")))
 
 (provide 'pg-flycheck)
