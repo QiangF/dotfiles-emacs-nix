6c7d5697aa698ba29fe55975eef64edf7b0f3cfc
Ben Whitley <dev@purplg.com>
2021-12-20 17:45:00 -0500

    Move relevent config into websockets package

--- .emacs.profiles.d/purplg/modules/pg-hass.el
+++ .emacs.profiles.d/purplg/modules/pg-hass.el
@@ -5,26 +5,29 @@
   :init
   ;; Dev packages aren't resolved automatically
   (straight-use-package 'request)
-  (straight-use-package 'websocket)
-
-  :config
-  ;; Initial config
   (setq hass-host "10.0.2.3")
   (setq hass-insecure t)
   (setq hass-apikey (auth-source-pass-get 'secret "home/hass/emacs-apikey"))
-  (setq hass-watched-entities '("switch.bedroom_light"))
-  (hass-setup)
-  
-  (setq pg/hass-original-theme current-theme)
+  :config
+  (hass-setup))
+
+(use-package hass-websockets
+  :after hass
+  :init
+  ;; Dev packages aren't resolved automatically
+  (straight-use-package 'websocket)
 
   ;; An automation just to "eat my own dogfood".
   ;; Changes Emacs theme based on the state of my bedroom light.
+  (setq hass-watched-entities '("switch.bedroom_light"))
+  (setq pg/hass-original-theme current-theme)
   (add-hook 'hass-entity-state-updated-functions
     (lambda (entity-id)
       (cond ((string= entity-id "switch.bedroom_light")
              (if (hass-switch-p entity-id)
                  (set-theme 'doom-one-light)
                  (set-theme pg/hass-original-theme))))))
+
   (hass-realtime-mode t))
 
 (provide 'pg-hass)
