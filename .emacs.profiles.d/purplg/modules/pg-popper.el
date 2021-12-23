0979b24aaf25e69794475fcfe0aa3dc70c975206
Ben Whitley <dev@purplg.com>
2021-12-23 18:28:24 -0500

    removed popper no-select function since it was merged

--- .emacs.profiles.d/purplg/modules/pg-popper.el
+++ .emacs.profiles.d/purplg/modules/pg-popper.el
@@ -17,15 +17,7 @@
                                     telega-chat-mode
                                     flymake-project-diagnostics-mode))
 
-  (defun popper-popup-at-bottom (buffer &optional _alist)
-    "Display popup-buffer BUFFER at the bottom of the screen."
-    (display-buffer-in-side-window
-     buffer
-     `((window-height . ,popper-window-height)
-       (side . bottom)
-       (slot . 1))))
-
-  (setq popper-display-function #'popper-popup-at-bottom)
+  (setq popper-display-function #'popper-display-popup-at-bottom)
 
   :config
   (popper-mode +1))
