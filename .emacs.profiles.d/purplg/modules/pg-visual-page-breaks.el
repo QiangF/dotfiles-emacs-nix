c842f7d3c2395e429cb96e034d105666ac133c32
Ben Whitley <dev@purplg.com>
2022-01-13 18:28:14 -0500

    Enabled page-break-lines to elisp modes

--- .emacs.profiles.d/purplg/modules/pg-visual-page-breaks.el
+++ .emacs.profiles.d/purplg/modules/pg-visual-page-breaks.el
@@ -1,6 +1,8 @@
 ;;; --- -*- lexical-binding: t; -*-
 
 (use-package page-break-lines
-  :straight t)
+  :straight t
+  :init
+  (add-hook 'emacs-lisp-mode-hook #'page-break-lines-mode))
 
 (provide 'pg-visual-page-breaks)
