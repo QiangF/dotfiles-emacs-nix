71a50f62f11dc86c402f9c2c256270e28d86d82f
Ben Whitley <dev@purplg.com>
2022-01-25 11:13:44 -0500

    Added Shackle package

--- .emacs.profiles.d/purplg/modules/pg-popper.el
+++ .emacs.profiles.d/purplg/modules/pg-popper.el
@@ -13,6 +13,7 @@
                                     "^\\*xref.*\\*$"
                                     "^\\*eshell.*\\*$" eshell-mode
                                     "^\\*vterm.*\\*$" vterm-mode
+                                    "^\\*hass-dash.*\\*$" hass-dash-mode
                                     flycheck-error-list-mode
                                     compilation-mode
                                     rustic-compilation-mode
@@ -21,8 +22,17 @@
                                     telega-chat-mode
                                     flymake-project-diagnostics-mode))
 
+  (setq popper-display-control nil)
   (setq popper-display-function #'popper-display-popup-at-bottom)
 
   (popper-mode +1))
+
+(use-package shackle
+  :straight t
+  :after popper
+  :init
+  (setq shackle-rules '((hass-dash-mode :align left :size 0.3)))
+  :config
+  (shackle-mode +1))
   
 (provide 'pg-popper)
