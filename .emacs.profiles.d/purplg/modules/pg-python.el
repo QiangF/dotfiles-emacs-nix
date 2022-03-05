83a9f2773979f223481ce22dbe4d16feb4e4c4d7
Ben Whitley <dev@purplg.com>
2022-03-04 21:11:05 -0500

    fmt python

--- .emacs.profiles.d/purplg/modules/pg-python.el
+++ .emacs.profiles.d/purplg/modules/pg-python.el
@@ -2,14 +2,17 @@
 (require 'pg-keybinds)
 (require 'pg-treesitter)
 
-(use-package python
+(use-package python :after corfu
   :config
-  (use-package python :after corfu :config (add-hook 'python-mode-hook #'corfu-mode))
-  (use-package python :after tree-sitter :config (add-hook 'python-mode-hook #'tree-sitter-mode)))
+  (add-hook 'python-mode-hook #'corfu-mode))
+
+(use-package python :after tree-sitter
+  :config
+  (add-hook 'python-mode-hook #'tree-sitter-mode))
 
 (use-package python :after eglot
   :config
-  (add-to-list 'python-mode-hook 'eglot-ensure)
+  (add-hook 'python-mode-hook 'eglot-ensure)
   (add-to-list 'eglot-server-programs
     `(python-mode . ("pyls" "-v" "--tcp" "--host" "localhost" "--port" :autoport))))
 
