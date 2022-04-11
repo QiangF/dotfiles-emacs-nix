5fcdc4e508411aa522c0d2ce9460f3d53b9fbc68
Ben Whitley <dev@purplg.com>
2022-04-10 22:43:49 -0400

    tempel instead of yasnippet

--- .emacs.profiles.d/purplg/modules/pg-snippets.el
+++ .emacs.profiles.d/purplg/modules/pg-snippets.el
@@ -1,6 +1,7 @@
 ;;; --- -*- lexical-binding: t; -*-
 
 (use-package yasnippet
+  :disabled
   :straight t
   :defer t
   :init
@@ -9,7 +10,13 @@
   (push (expand-file-name "snippets" pg/config-dir) yas-snippet-dirs))
 
 (use-package yasnippet-snippets
+  :disabled
   :straight t
   :after yasnippet)
+
+(use-package tempel
+  :straight t
+  :init
+  (setq tempel-path (expand-file-name "templates" pg/config-dir)))
 
 (provide 'pg-snippets)
