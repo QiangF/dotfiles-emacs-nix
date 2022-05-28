01e8be911463d2cfaeaf5784369b9c36c52e0893
Ben Whitley <dev@purplg.com>
2022-05-28 13:57:00 -0400

    Updated keybinds

--- .emacs.profiles.d/purplg/modules/pg-keybinds.el
+++ .emacs.profiles.d/purplg/modules/pg-keybinds.el
@@ -11,7 +11,7 @@
     (interactive)
     (transpose-lines 1)
     (forward-line -2))
-  
+
 (defun move-line-down ()
     (interactive)
     (forward-line 1)
@@ -30,7 +30,7 @@
   (interactive)
   (evil-yank (point-min) (point-max))
   (message "Yanked entire buffer."))
-  
+
 (use-package which-key
   :straight t
   :init
@@ -44,11 +44,11 @@
    :states '(normal visual)
    :prefix "SPC"
    :non-normal-prefix "M-SPC")
-  
+
   (general-create-definer pg/local-leader
    :states '(normal visual)
    :prefix "S-SPC")
-  
+
   (pg/leader
    "m" '(:ignore t :wk "local")
    "s" '(:ignore t :wk "search")
@@ -67,26 +67,26 @@
    "t f" #'(display-fill-column-indicator-mode :wk "fill-column")
    "t n" #'(display-line-numbers-mode :wk "line numbers")
    "t w" #'(whitespace-mode :wk "whitespace")
-  
+
    "f f" #'(find-file :wk "find")
    "f s" #'(save-buffer :wk "save")
    "f S" #'(save-all-buffers :wk "save all")
    "f ." #'(find-file-at-point :wk "this")
    "f m" #'(pg/open-module :wk "module")
-  
+
    "h k" #'(describe-key :wk "key")
    "h p" #'(describe-package :wk "package")
-  
+
    "q w" #'(delete-window :wk "window")
    "q b" #'(kill-this-buffer :wk "buffer")
    "q q" #'(save-buffers-kill-terminal :wk "really quit?")
-  
+
    "w d" #'(delete-window :wk "delete")
    "w D" #'(kill-buffer-and-window :wk "delete with buffer")
    "w s" #'(split-window-below :wk "split below")
    "w v" #'(split-window-right :wk "split right")
    "w =" #'(balance-windows :wk "balance")
-  
+
    "b b" #'(switch-to-buffer :wk "open")
    "b B" #'(switch-to-buffer :wk "open")
    "b d" #'(kill-this-buffer :wk "delete")
@@ -94,9 +94,9 @@
    "b r" #'(revert-buffer :wk "revert")
    "b s" #'(open-scratch-buffer :wk "scratch")
    "b m" #'(view-echo-area-messages :wk "messages")
-  
+
    ";" #'eval-expression)
-  
+
   ;; Unbind SPC in Dired mode
   ;; Dired takes precendence for the ~SPC~ key. Don't like that
   (general-define-key
@@ -133,39 +133,46 @@
               :after #'evil-scroll-line-to-center)
   (advice-add 'evil-backward-section-begin
               :after #'evil-scroll-line-to-center)
-  
+
   ;; Keep cursor in place when opening a fold
   (advice-add 'evil-open-fold
               :around (lambda (inner &rest _)  
                         (save-excursion (funcall inner))))
 
-  (pg/leader
+  (general-define-key
+   :states 'visual
+   ":" #'comment-dwim)
+  (general-define-key
    :states 'normal
-   "b p" #'(evil-prev-buffer :wk "previous")
-   "b n" #'(evil-next-buffer :wk "next")
-   "b N" #'(evil-buffer-new :wk "new"))
+   ";" #'comment-line)
 
-  (general-define-key
-   :states 'normal
-   "z c" #'evil-close-fold-below
-   "z m" #'hs-hide-level
-   "z C" #'evil-close-fold)
-    
-  (general-define-key
-   :states 'normal
-   "M-j" #'move-line-down
-   "M-k" #'move-line-up
-   "C-j" #'evil-forward-section-begin
-   "C-k" #'evil-backward-section-begin)
-      
-   ;; I often press =C-w C-h= to go left (for example) instead of =C-w h= so I'll just bind both.
-  (general-define-key
-   :states 'normal
-   "C-w C-h" #'evil-window-left
-   "C-w C-j" #'evil-window-down
-   "C-w C-k" #'evil-window-up
-   "C-w C-l" #'evil-window-right
-   "C-<tab>" #'evil-switch-to-windows-last-buffer))
+ (pg/leader
+  :states 'normal
+  "b p" #'(evil-prev-buffer :wk "previous")
+  "b n" #'(evil-next-buffer :wk "next")
+  "b N" #'(evil-buffer-new :wk "new"))
+
+ (general-define-key
+  :states 'normal
+  "z c" #'evil-close-fold-below
+  "z m" #'hs-hide-level
+  "z C" #'evil-close-fold)
+
+ (general-define-key
+  :states 'normal
+  "M-j" #'move-line-down
+  "M-k" #'move-line-up
+  "C-j" #'evil-forward-section-begin
+  "C-k" #'evil-backward-section-begin)
+
+  ;; I often press =C-w C-h= to go left (for example) instead of =C-w h= so I'll just bind both.
+ (general-define-key
+  :states 'normal
+  "C-w C-h" #'evil-window-left
+  "C-w C-j" #'evil-window-down
+  "C-w C-k" #'evil-window-up
+  "C-w C-l" #'evil-window-right
+  "C-<tab>" #'evil-switch-to-windows-last-buffer))
 
 (use-package evil-surround
   :straight t
