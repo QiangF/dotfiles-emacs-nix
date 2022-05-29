148a17c6a393a2ff7c1fecb0a79415e535e25ddc
Ben Whitley <dev@purplg.com>
2022-05-29 12:59:08 -0400

    Close vterm window when it exits

--- .emacs.profiles.d/purplg/modules/pg-term.el
+++ .emacs.profiles.d/purplg/modules/pg-term.el
@@ -12,6 +12,10 @@
   :defer t
   :straight t
   :init
+  ;; Close the window when vterm exits
+  (add-hook 'vterm-exit-functions
+            (lambda (buffer _event)
+              (delete-window (get-buffer-window buffer))))
   (pg/leader
    "o t" #'(vterm-other-window :wk "terminal")))
 
