9a8e771ae53ce43f03e4a9301671bf566b7bc274
Ben Whitley <dev@purplg.com>
2022-07-28 18:05:48 -0400

    Set line number width to 3 characters min

--- .emacs.profiles.d/purplg/modules/pg-line-numbers.el
+++ .emacs.profiles.d/purplg/modules/pg-line-numbers.el
@@ -2,6 +2,7 @@
 
 (setq-default display-line-numbers-type 'visual)
 (setq-default display-line-numbers-current-absolute t)
+(setq-default display-line-numbers-width 3)
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
 (with-eval-after-load 'org-mode
   (add-hook 'org-mode-hook #'display-line-numbers-mode))
