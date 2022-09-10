635837dcd4846cd9a3363fec88f64a902b78b370
Ben Whitley <dev@purplg.com>
2022-09-10 13:50:26 -0400

    Switch to ef-winter theme

--- .emacs.profiles.d/purplg/early-init.el
+++ .emacs.profiles.d/purplg/early-init.el
@@ -1,18 +1,1 @@
 (setq package-enable-at-startup nil)
-
-(defvar current-theme nil)
-
-(defun set-theme (theme-name)
-  (setq current-theme theme-name)
-  (load-theme current-theme t))
-
-(setq modus-themes-italic-constructs t)
-(setq modus-themes-bold-constructs t)
-(setq modus-themes-region '(bg-only))
-(setq modus-themes-syntax '(green-strings))
-(setq modus-themes-paren-match '(bold intense))
-(setq modus-themes-mode-line '(borderless accented))
-(setq modus-themes-prompts '(intense bold))
-(setq modus-themes-org-blocks 'gray-background)
-(setq modus-themes-completions '((popup . (accented intense))))
-(set-theme 'modus-vivendi)
--- .emacs.profiles.d/purplg/modules/pg-theme.el
+++ .emacs.profiles.d/purplg/modules/pg-theme.el
@@ -1,11 +1,22 @@
 ;;; --- -*- lexical-binding: t; -*-
 
+(defvar current-theme nil)
+
+(defun set-theme (theme-name)
+  (setq current-theme theme-name)
+  (load-theme current-theme t))
+
 (use-package modus-themes
   :bind ("<f5>" . modus-themes-toggle)
   :config
   (advice-add 'modus-themes-toggle
               :after
               (lambda (&rest _) (setq current-theme (modus-themes--current-theme)))))
+
+(use-package ef-themes
+  :straight t
+  :init
+  (set-theme 'ef-winter))
 
 (use-package org-modern
   :straight t
