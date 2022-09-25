fe25e47bcba7d6a036ace45e801ccc5715888f1c
Ben Whitley <dev@purplg.com>
2022-09-25 14:26:09 -0400

    Fixed ahs not working in daemon mode

--- .emacs.profiles.d/purplg/modules/pg-theme.el
+++ .emacs.profiles.d/purplg/modules/pg-theme.el
@@ -8,27 +8,6 @@
 
 (use-package modus-themes
   :bind ("<f5>" . modus-themes-toggle)
-  :init
-  (with-eval-after-load 'auto-highlight-symbol
-    ;; Set inner border for symbol highlights
-    (set-face-attribute 'ahs-face nil
-                        :box `(:line-width (-1 . -1)
-                               :color ,(face-attribute 'cursor :background) ;; Set the border color the same as the cursor
-                               :style nil)
-                        :background 'unspecified :foreground 'unspecified)
-
-    ;; Make the highlight at point look the same as any highlight.
-    (set-face-attribute 'ahs-plugin-default-face nil
-                        :background 'unspecified
-                        :foreground 'unspecified)
-
-    ;; Do not show any symbol highlights when buffer isn't focused
-    (set-face-attribute 'ahs-face-unfocused nil
-                        :box nil)
-
-    (set-face-attribute 'ahs-plugin-default-face-unfocused nil
-                        :background 'unspecified
-                        :foreground 'unspecified))
   :config
   (advice-add 'modus-themes-toggle
               :after
@@ -38,6 +17,31 @@
   :straight t
   :init
   (set-theme 'ef-winter)
+  (with-eval-after-load 'auto-highlight-symbol
+    (let ((set-ahs-faces
+           (lambda ()
+             ;; Set inner border for symbol highlights
+             (set-face-attribute 'ahs-face nil
+                                 :box `(:line-width (-1 . -1)
+                                                    :color ,(face-attribute 'cursor :background) ;; Set the border color the same as the cursor
+                                                    :style nil)
+                                 :background 'unspecified :foreground 'unspecified)
+
+             ;; Make the highlight at point look the same as any highlight.
+             (set-face-attribute 'ahs-plugin-default-face nil
+                                 :background 'unspecified
+                                 :foreground 'unspecified)
+
+             ;; Do not show any symbol highlights when buffer isn't focused
+             (set-face-attribute 'ahs-face-unfocused nil
+                                 :box nil)
+
+             (set-face-attribute 'ahs-plugin-default-face-unfocused nil
+                                 :background 'unspecified
+                                 :foreground 'unspecified))))
+      (funcall set-ahs-faces)
+      (add-hook 'server-after-make-frame-hook set-ahs-faces)))
+
   (with-eval-after-load 'hl-line
     (set-face-background 'hl-line "#130e1b")))
 
