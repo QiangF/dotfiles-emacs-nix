70b049009774e04406ea5fb882542d00829ffa48
Ben Whitley <dev@purplg.com>
2022-09-25 14:24:44 -0400

    Updated Ement config, include fixing `ement-after-initial-sync-hook`

--- .emacs.profiles.d/purplg/modules/pg-matrix.el
+++ .emacs.profiles.d/purplg/modules/pg-matrix.el
@@ -10,30 +10,24 @@
     (ement-connect
       :user-id (concat "@" (auth-source-pass-get "username" "matrix.org") ":matrix.org")
       :password (auth-source-pass-get 'secret "matrix.org")))
-
-  :config
-  ;; Don't open room list automatically
-  (setq ement-after-initial-sync-hook nil)
-  (pg/ement-connect)
   
-  ;; Connect to Matrix if not connected.
-  (advice-add
-   'ement-list-rooms
-   :before (lambda () (unless ement-sessions (pg/ement-connect))))
-  
-  ;; Global Keybinds
   (pg/leader
    :states 'normal
    "o m" #'(ement-list-rooms :wk "Matrix"))
 
-  ;; Ement.el Keybinds
+  :config
+  ;; Don't open room list automatically
+  (remove-hook 'ement-after-initial-sync-hook 'ement-list-rooms)
+
   (general-define-key
    :states '(normal insert visual)
    :keymaps 'ement-room-mode-map
    "<return>" #'ement-room-send-message
    "e"        #'ement-room-send-emote
    "r"        #'ement-room-send-reply
-   "d"        #'ement-room-delete-message))
- 
+   "d"        #'ement-room-delete-message)
+
+  (pg/ement-connect))
+
   
 (provide 'pg-matrix)
