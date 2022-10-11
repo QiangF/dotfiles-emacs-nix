14fdbb22ed8cc88b3f5797fbf78ea901ff3fc07c
Ben Whitley <dev@purplg.com>
2022-10-11 08:54:16 -0400

    Fixed undo tree files

--- .emacs.profiles.d/purplg/modules/pg-undo.el
+++ .emacs.profiles.d/purplg/modules/pg-undo.el
@@ -4,7 +4,11 @@
 (use-package undo-tree
   :straight t
   :init
-  (add-hook 'evil-local-mode-hook #'turn-on-undo-tree-mode)
-  (global-undo-tree-mode))
+  (add-hook 'text-mode-hook #'turn-on-undo-tree-mode)
+  (add-hook 'prog-mode-hook #'turn-on-undo-tree-mode)
+  (add-hook 'conf-mode-hook #'turn-on-undo-tree-mode)
+  (let ((undo-dir (expand-file-name "undo" user-emacs-directory)))
+    (unless (f-directory-p undo-dir) (make-directory undo-dir))
+    (setq undo-tree-history-directory-alist `(("." . ,undo-dir)))))
 
 (provide 'pg-undo)
