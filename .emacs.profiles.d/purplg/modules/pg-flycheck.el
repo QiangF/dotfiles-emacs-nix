f476ebfaf0355e86c6b6dd2ef740008154c49bfc
Ben Whitley <dev@purplg.com>
2022-10-12 17:48:42 -0400

    updated flycheck and flymake keybinds

--- .emacs.profiles.d/purplg/modules/pg-flycheck.el
+++ .emacs.profiles.d/purplg/modules/pg-flycheck.el
@@ -2,14 +2,14 @@
 (require 'pg-lsp)
 
 (use-package flycheck
-  :disabled
   :straight t
   :config
   (add-hook 'lsp-mode-hook #'flycheck-mode)
   (general-define-key
+   :keymaps 'flycheck-mode-map
    :states 'normal
-   "M-h" #'(flycheck-previous-error :wk "prev error")
-   "M-l" #'(flycheck-next-error :wk "next error")))
+   "M-k" #'(flycheck-previous-error :wk "prev error")
+   "M-j" #'(flycheck-next-error :wk "next error")))
  
 (use-package flymake
   ;; Next commit breaks compat with Emacs 28
@@ -17,10 +17,11 @@
   :straight (:host github
              :repo "emacs-straight/flymake"
              :commit "85076b79ee5c576c2b2ca28c6358045dee23ec8e")
-  :hook (eglot--managed-mode . flymake-mode)
+  ;;:hook (eglot--managed-mode . flymake-mode)
   :config
   (setq elisp-flymake-byte-compile-load-path load-path)
   (general-define-key
+   :keymaps 'flymake-mode-map
    :states 'normal
    "M-k" #'(flymake-goto-prev-error :wk "prev error")
    "M-j" #'(flymake-goto-next-error :wk "next error")))
