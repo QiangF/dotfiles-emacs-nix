9e592113f2010bebc55a4ae3753d786aea555361
Ben Whitley <dev@purplg.com>
2022-12-29 18:45:11 -0500

    Updated hass for 3.0

--- .config/emacs/modules/pg-hass.el
+++ .config/emacs/modules/pg-hass.el
@@ -1,9 +1,6 @@
 ;;; --- -*- lexical-binding: t; -*-
 (require 'pg-theme)
 
-(use-package websocket
-  :straight t)
-
 (use-package hass
   :straight t
   :after pass
@@ -12,213 +9,85 @@
   (setq hass-insecure t)
   (setq hass-apikey (auth-source-pass-get 'secret "home/hass/emacs-apikey"))
   (pg/leader
-   :states 'normal
-   "a" #'(:ignore t :wk "automation")
-   "a c" #'(hass-call-service :wk "Call service"))
+    :states 'normal
+    "a" #'(:ignore t :wk "automation")
+    "a c" #'(hass-call-service :wk "Call service")
+    "a d" #'(hass-dash-open :wk "Call service"))
 
   (add-to-list 'popper-reference-buffers "^\\*hass-dash.*\\*$")
   (add-to-list 'popper-reference-buffers 'hass-dash-mode)
+  
+  (setq hass-dash-layouts '((default . ((hass-dash-group :title "Desktop" :format "%t\n%v\n"
+                                                         (hass-dash-toggle :entity-id "switch.desktop" :confirm t)
+                                                         (hass-dash-button :entity-id "media_player.mpd" :label "MEDIA")
+                                                         (hass-dash-state :entity-id "sensor.desktop_cpu" :label "CPU")
+                                                         (hass-dash-state :entity-id "sensor.desktop_ram" :label "RAM"))
+                                        (hass-dash-group :title "Laptop" :format "%t\n%v\n"
+                                                         (hass-dash-toggle :entity-id "switch.laptopbig" :label "Power" :confirm t)
+                                                         (hass-dash-state :entity-id "sensor.laptopbig_cpu" :label "CPU")
+                                                         (hass-dash-state :entity-id "sensor.laptopbig_ram" :label "RAM"))
+                                        (hass-dash-group :title "Phone" :format "%t\n%v\n"
+                                                         (hass-dash-state :entity-id "sensor.pixel_7_pro_battery_level" :label "Battery" :format "%t: %v%%\n")
+                                                         (hass-dash-state :entity-id "sensor.pixel_7_pro_battery_state" :label "Bat state?")
+                                                         (hass-dash-state :entity-id "sensor.pixel_7_pro_phone_state" :label "Phone")
+                                                         (hass-dash-state :entity-id "sensor.pixel_7_pro_media_session" :label "Media"))
+                                        (hass-dash-group :title "TV" :format "%t\n%v\n"
+                                                         (hass-dash-button :entity-id "media_player.shield" :icon "" :label " Toggle"))
+                                        (hass-dash-group :title "Bedroom" :format "%t\n%v\n"
+                                                         (hass-dash-toggle :entity-id "light.bedroom")
+                                                         (hass-dash-toggle :entity-id "fan.bedroom"))
+                                        (hass-dash-group :title "Vacuum"
+                                                         (hass-dash-state :entity-id "vacuum.valetudo_vacuum"
+                                                                          :format "%v\n")
+                                                         (hass-dash-button :entity-id "vacuum.valetudo_vacuum"
+                                                                           :service "vacuum.start"
+                                                                           :format "%[%t%]\n"
+                                                                           :label "Clean")
+                                                         (hass-dash-button :entity-id "vacuum.valetudo_vacuum"
+                                                                           :service "vacuum.return_to_base"
+                                                                           :format "%[%t%]\n"
+                                                                           :label "Return to base")
+                                                         (hass-dash-button :entity-id "vacuum.valetudo_vacuum"
+                                                                           :service "vacuum.locate"
+                                                                           :format "%[%t%]\n"
+                                                                           :label "Locate"))))
+                            (testing . ((hass-dash-group :title "" :format "%v"
+                                                         (hass-dash-toggle :entity-id "input_boolean.hass_mode_test")
+                                                         (hass-dash-button :entity-id "scene.test_scene"))))
+                            (broken . ((hass-dash-group :title "Group 1"
+                                                        (hass-dash-toggle :entity-id "input_boolean.hass_mode_test")
+                                                        (hass-dash-group :title "Group 1 Subgroup 1"
+                                                                         (hass-dash-toggle :entity-id "input_boolean.hass_mode_test"))
+                                                        (hass-dash-group :title "Group 1 Subgroup 2"
+                                                                         (hass-dash-toggle :entity-id "input_boolean.hass_mode_test")))
+                                       (hass-dash-group :title "Group 2"
+                                                        (hass-dash-toggle :entity-id "input_boolean.hass_mode_test")
+                                                        (hass-dash-group :title "Group 2 Subgroup 1"
+                                                                         (hass-dash-toggle :entity-id "input_boolean.hass_mode_test"))
+                                                        (hass-dash-group :title "Group 2 Subgroup 2"
+                                                                         (hass-dash-toggle :entity-id "input_boolean.hass_mode_tes")))))))
+
+  (defun hass-capf (&optional interactive)
+    (interactive (list t))
+    (if interactive
+        (let ((completion-at-point-functions (list #'hass-capf)))
+          (completion-at-point))
+      (let ((bounds (bounds-of-thing-at-point 'symbol)))
+        (list (car bounds)
+              (cdr bounds)
+              (mapcar #'car hass--available-entities)))))
 
   :config
-  (hass-setup))
-
-(use-package hass
-  :after websocket
-  :init
-  (cl-defun pg/hass-dash-widget-formatter (label state icon &optional icon-formatter label-formatter state-formatter)
-    (concat (when icon (funcall icon-formatter icon))
-            (when state (funcall state-formatter state))
-            (funcall label-formatter label)))
-
-  (defun pg/hass-dash-state-formatter (label)
-    (concat " " (propertize label
-                  'face 'all-the-icons-purple)))
-
-  (setq hass-dash-default-state-formatter #'pg/hass-dash-state-formatter)
-
-  (defun pg/percent-suffix (str)
-    (concat " " (propertize (concat str "%")
-                  'face 'all-the-icons-purple)))
-
-  (defun pg/laptop-toggle-widget-formatter (_label state icon
-                                            label-formatter _state-formatter icon-formatter)
-    (if (string= "on" state)
-      (concat (when icon (funcall icon-formatter icon))
-              (funcall label-formatter "Turn off"))
-      "Powered off"))
-
-  (defun pg/hide-unavailable (widget)
-    (string= "unavailable" (hass-state-of (car widget))))
-
-  (defun pg/hass-dash-light-label-formatter (label)
-    (propertize label 'face 'font-lock-function-name-face))
-
-  (defun pg/hass-dash-light-state-formatter (label)
-    (concat ": "
-      (propertize label 'face 'font-lock-keyword-face)))
-
-  (defun pg/hass-dash-light-icon-formatter (icon)
-    (concat icon "|"))
-
-  (setq pg/hass-dash-group-light
-    '("Light" .
-      (("switch.bedroom_light"
-        :label "Bedroom"
-        :widget-formatter (lambda (label state icon label-formatter state-formatter icon-formatter)
-                            (concat (when icon (funcall icon-formatter icon))
-                                    (funcall label-formatter label)
-                                    (when state (funcall state-formatter state))))
-        :label-formatter pg/hass-dash-light-label-formatter
-        :state-formatter pg/hass-dash-light-state-formatter
-        :icon-formatter pg/hass-dash-light-icon-formatter))))
-
-  (setq pg/hass-dash-group-desktop
-    '("Desktop" .
-      (("switch.desktop"
-          :label "Turn "
-          :confirm (lambda (entity-id)
-                     (if (hass-switch-p entity-id)
-                         (y-or-n-p "Turn off? ")
-                         t))
-          :state-formatter (lambda (state) (if (string= "off" state) "on" "off")))
-       ("sensor.desktop_cpu"
-          :label "CPU"
-          :icon nil
-          :service nil
-          :hide-fn pg/hide-unavailable
-          :state-formatter pg/percent-suffix)
-       ("sensor.desktop_ram"
-          :label "MEM"
-          :icon nil
-          :service nil
-          :hide-fn pg/hide-unavailable
-          :state-formatter pg/percent-suffix))))
-
-  (setq pg/hass-dash-group-framework
-    '("Framework" .
-      (("switch.framework_mqtt"
-          :label "Framework"
-          :widget-formatter pg/laptop-toggle-widget-formatter
-          :confirm (lambda (entity-id)
-                     (if (hass-switch-p entity-id)
-                         (y-or-n-p "Turn off? ")
-                         t)))
-       ("sensor.framework_battery0"
-          :label "BAT"
-          :icon nil
-          :service nil
-          :hide-fn pg/hide-unavailable
-          :state-formatter pg/percent-suffix)
-       ("sensor.framework_cpu"
-          :label "CPU"
-          :icon nil
-          :service nil
-          :hide-fn pg/hide-unavailable
-          :state-formatter pg/percent-suffix)
-       ("sensor.framework_ram"
-          :label "MEM"
-          :icon nil
-          :hide-fn pg/hide-unavailable
-          :state-formatter pg/percent-suffix))))
-
-  (setq pg/hass-dash-group-laptopbig
-    '("LaptopBig" .
-      (("switch.laptopbig_mqtt"
-          :label "LaptopBig"
-          :confirm (lambda (entity-id)
-                     (if (hass-switch-p entity-id)
-                         (y-or-n-p "Turn off? ")
-                         t))
-          :widget-formatter pg/laptop-toggle-widget-formatter)
-       ("sensor.laptopbig_battery0"
-          :label "BAT"
-          :icon nil
-          :service nil
-          :hide-fn pg/hide-unavailable
-          :state-formatter pg/percent-suffix)
-       ("sensor.laptopbig_cpu"
-          :label "CPU"
-          :icon nil
-          :service nil
-          :hide-fn pg/hide-unavailable
-          :state-formatter pg/percent-suffix)
-       ("sensor.laptopbig_ram"
-          :label "MEM"
-          :icon nil
-          :service nil
-          :hide-fn pg/hide-unavailable
-          :state-formatter pg/percent-suffix))))
-
-  (setq pg/hass-dash-group-test
-    '("Test" .
-      (("input_boolean.hass_mode_test")
-       ("input_boolean.hass_mode_test"
-          :label "Turn off Hass mode test"
-          :confirm t
-          :service "input_boolean.turn_off"
-          :state nil)
-       ("scene.test_scene"
-          :state nil))))
-
-  (setq pg/hass-dash-group-phone
-    '("Phone" .
-       (("device_tracker.oneplus_7t_pro"
-         :label "Location")
-        ("sensor.oneplus_7t_pro_media_session"
-         :label "Media")
-        ("sensor.oneplus_7t_pro_phone_state"
-         :label "Call")
-        ("sensor.oneplus_7t_pro_battery_level"
-         :label "BAT"
-         :state-formatter
-         (lambda (state)
-           (concat " " (propertize (concat state "%")
-                        'face 'all-the-icons-purple))))
-        ("sensor.oneplus_7t_pro_battery_state"
-         :label ""
-         :icon nil
-         :state-formatter (lambda (state) state)))))
-
-  (setq pg/hass-dash-group-vacuum
-   '("Vacuum" .
-     (("vacuum.valetudo_vacuum"
-         :label "State")
-      ("vacuum.valetudo_vacuum"
-         :label "Clean"
-         :state nil)
-      ("vacuum.valetudo_vacuum"
-         :label "Return to base"
-         :service "vacuum.return_to_base"
-         :state nil)
-      ("vacuum.valetudo_vacuum"
-         :label "Locate"
-         :service "vacuum.locate"
-         :state nil))))
-
-  (setq hass-dash-layout '(pg/hass-dash-group-light
-                           pg/hass-dash-group-desktop
-                           pg/hass-dash-group-framework
-                           pg/hass-dash-group-laptopbig
-                           pg/hass-dash-group-phone
-                           pg/hass-dash-group-vacuum
-                           pg/hass-dash-group-test))
-
   ;; An automation just to "eat my own dogfood".
   ;; Changes Emacs theme based on the state of my bedroom light.
   (add-to-list 'hass-tracked-entities "switch.bedroom_light")
   (setq pg/hass-original-theme current-theme)
   (add-hook 'hass-entity-state-changed-functions
-    (lambda (entity-id)
-      (when (equal entity-id "switch.bedroom_light")
-            (if (hass-switch-p entity-id)
-                (set-theme 'ef-light)
-              (set-theme pg/hass-original-theme)))))
-
-  (hass-websocket-mode t)
-
-  :config
-  (pg/leader
-   :states 'normal
-   "a d" #'(hass-dash-open :wk "dashboard")))
+            (lambda (entity-id)
+              (when (equal entity-id "switch.bedroom_light")
+                (if (hass-switch-p entity-id)
+                    (set-theme 'tsdh-light)
+                  (set-theme pg/hass-original-theme)))))
+  (hass-ensure))
 
 (provide 'pg-hass)
