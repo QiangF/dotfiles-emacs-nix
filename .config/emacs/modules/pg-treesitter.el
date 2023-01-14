e15b01243b3dba4a3c7f69a71356b5ac0a210001
Ben Whitley <dev@purplg.com>
2023-01-14 10:42:29 -0500

    Add native treesit support

--- .config/emacs/modules/pg-treesitter.el
+++ .config/emacs/modules/pg-treesitter.el
@@ -1,10 +1,58 @@
 ;;; --- -*- lexical-binding: t; -*-
 
+;; https://www.nathanfurnal.xyz/posts/building-tree-sitter-langs-emacs/
+(use-package treesit
+  :commands (treesit-install-language-grammar nf/treesit-install-all-languages)
+  :if (version< "29" emacs-version)
+  :init
+  (setq treesit-language-source-alist
+        '((bash . ("https://github.com/tree-sitter/tree-sitter-bash"))
+          (c . ("https://github.com/tree-sitter/tree-sitter-c"))
+          (cpp . ("https://github.com/tree-sitter/tree-sitter-cpp"))
+          (css . ("https://github.com/tree-sitter/tree-sitter-css"))
+          (go . ("https://github.com/tree-sitter/tree-sitter-go"))
+          (html . ("https://github.com/tree-sitter/tree-sitter-html"))
+          (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript"))
+          (json . ("https://github.com/tree-sitter/tree-sitter-json"))
+          (lua . ("https://github.com/Azganoth/tree-sitter-lua"))
+          (make . ("https://github.com/alemuller/tree-sitter-make"))
+          (ocaml . ("https://github.com/tree-sitter/tree-sitter-ocaml" "ocaml/src" "ocaml"))
+          (python . ("https://github.com/tree-sitter/tree-sitter-python"))
+          (php . ("https://github.com/tree-sitter/tree-sitter-php"))
+          (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "typescript/src" "typescript"))
+          (ruby . ("https://github.com/tree-sitter/tree-sitter-ruby"))
+          (rust . ("https://github.com/tree-sitter/tree-sitter-rust"))
+          (sql . ("https://github.com/m-novikov/tree-sitter-sql"))
+          (toml . ("https://github.com/tree-sitter/tree-sitter-toml"))
+          (zig . ("https://github.com/GrayJack/tree-sitter-zig"))))
+
+  (setq major-mode-remap-alist
+   '((sh-mode         . bash-ts-mode)
+     (c-mode          . c-ts-mode)
+     (c++-mode        . c++-ts-mode)
+     (csharp-mode     . csharp-ts-mode)
+     (cmake-mode      . cmake-ts-mode)
+     (css-mode        . css-ts-mode)
+     (dockerfile-mode . dockerfile-ts-mode)
+     (go-mode         . go-ts-mode)
+     (java-mode       . java-ts-mode)
+     (javascript-mode . js-ts-mode)
+     (js-json-mode    . json-ts-mode)
+     (python-mode     . python-ts-mode)
+     (ruby-mode       . ruby-ts-mode)
+     (rust-mode       . rust-ts-mode)
+     (conf-toml-mode  . toml-ts-mode)
+     (rjsx-mode       . tsx-ts-mode)
+     (typescript-mode . typescript-ts-mode)
+     (yaml-mode       . yaml-ts-mode))))
+
 (use-package tree-sitter
-  :if (string= "x86_64" (car (split-string system-configuration "-")))
+  :if (and (version< emacs-version "29")
+           (string= "x86_64" (car (split-string system-configuration "-"))))
   :straight t)
 
 (use-package tree-sitter-langs
+  :if (version< emacs-version "29")
   :straight t
   :after tree-sitter
   :config
