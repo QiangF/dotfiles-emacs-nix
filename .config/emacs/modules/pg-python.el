382cccc767f4c7ef018a2cf5c7f2b23bf16c35cc
Ben Whitley <dev@purplg.com>
2023-01-14 10:42:17 -0500

    Add python-ts-mode config

--- .config/emacs/modules/pg-python.el
+++ .config/emacs/modules/pg-python.el
@@ -3,16 +3,20 @@
 (require 'pg-treesitter)
 
 (with-eval-after-load 'corfu
-  (add-hook 'python-mode-hook #'corfu-mode))
+  (add-hook 'python-mode-hook #'corfu-mode)
+  (add-hook 'python-ts-mode-hook #'corfu-mode))
 
 (with-eval-after-load 'tree-sitter
   (add-hook 'python-mode-hook #'tree-sitter-mode))
 
 (with-eval-after-load 'eglot
-  (add-hook 'python-mode-hook #'eglot-ensure))
+  (add-hook 'python-mode-hook #'eglot-ensure)
+  (add-hook 'python-ts-mode-hook #'eglot-ensure)
+  (add-to-list 'eglot-server-programs '(python-ts-mode . ("jedi-language-server"))))
 
 (with-eval-after-load 'lsp-mode
-  (add-hook 'python-mode-hook #'lsp))
+  (add-hook 'python-mode-hook #'lsp)
+  (add-hook 'python-ts-mode-hook #'lsp))
 
 (use-package pyvenv
  :straight t
