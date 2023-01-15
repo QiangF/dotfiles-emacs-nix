b539c3ce00fc3a86ab99617661087b21554443a2
Ben Whitley <dev@purplg.com>
2023-01-15 18:39:06 -0500

    Removed chemacs2

--- .emacs-profiles.el
+++
@@ -1,4 +0,0 @@
-(("purplg" . ((user-emacs-directory . "~/.emacs.profiles.d/purplg")))
- ("vanilla" . ((user-emacs-directory . "~/.emacs.profiles.d/vanilla")))
- ("straight" . ((user-emacs-directory . "~/.emacs.profiles.d/straight")))
- ("doom" . ((user-emacs-directory . "~/.emacs.profiles.d/doom"))))
