7ed4248a7ce04362c81d0bb847856baad7b763bf
Ben Whitley <dev@purplg.com>
2023-01-15 15:51:42 -0500

    enable undo-tree everywhere

--- .config/emacs/modules/pg-undo.el
+++ .config/emacs/modules/pg-undo.el
@@ -3,9 +3,10 @@
 
 (use-package undo-tree
   :init
-  (add-hook 'text-mode-hook #'turn-on-undo-tree-mode)
-  (add-hook 'prog-mode-hook #'turn-on-undo-tree-mode)
-  (add-hook 'conf-mode-hook #'turn-on-undo-tree-mode)
+  (dolist (hook '(text-mode-hook
+                  prog-mode-hook
+                  conf-mode-hook))
+    (add-hook hook (lambda () (undo-tree-mode 1))))
   (let ((undo-dir (expand-file-name "undo" user-emacs-directory)))
     (unless (f-directory-p undo-dir) (make-directory undo-dir))
     (setq undo-tree-history-directory-alist `(("." . ,undo-dir)))))
