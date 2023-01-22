41f8c6214080b665a8765d1e8931319f5c3661b3
Ben Whitley <dev@purplg.com>
2023-01-22 18:05:40 -0500

    new org-agenda config

--- .config/emacs/modules/pg-org.el
+++ .config/emacs/modules/pg-org.el
@@ -11,17 +11,16 @@
   (add-hook 'org-mode-hook #'visual-line-mode)
 
   ;; Tweaks
-  (setq org-return-follows-link t)                ;; Press Enter to follow link under point
-  (setq org-adapt-indentation nil)                ;; Stop putting indents everywhere
-  (setq org-edit-src-content-indentation 0)       ;; Fixes indenting entire src block on enter
-  (setq org-src-preserve-indentation t)           ;; Stop annoying bug with indenting elisp in a code block
-  (setq org-confirm-babel-evaluate nil)           ;; Don't ask for confirmation when executing a codeblock
+  (setq org-return-follows-link t) ;; Press Enter to follow link under point
+  (setq org-adapt-indentation nil) ;; Stop putting indents everywhere
+  (setq org-edit-src-content-indentation 0) ;; Fixes indenting entire src block on enter
+  (setq org-src-preserve-indentation t) ;; Stop annoying bug with indenting elisp in a code block
+  (setq org-confirm-babel-evaluate nil) ;; Don't ask for confirmation when executing a codeblock
   (setq org-src-window-setup 'split-window-below) ;; Don't hide other windows when using `org-edit-special'
+  (setq org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
 
-  ;; Agenda
   (when (file-exists-p "~/.org") (setq org-directory "~/.org"))
   (when-let (org-pc (expand-file-name "PC.org" org-directory))
-    (setq org-agenda-files `(,org-pc))
     (setq initial-buffer-choice
           (lambda ()
             (let ((buffer (find-file org-pc)))
@@ -31,36 +30,41 @@
   ;; Capture
   (setq org-capture-project-file "project.org")
   (setq org-capture-templates
-        '(("w" "Work"
-            entry (file+headline "~/.org/Work.org" "Tasks")
-            "* TODO %?\n %i\n")
-          ("p" "Current project"
-            entry (file+headline (lambda () (expand-file-name org-capture-project-file (vc-root-dir))) "Tasks")
-            "* TODO %?\n%i\n%a" :prepend t)
-          ("s" "Session"
-            entry (file+headline "~/.org/PC.org" "Session")
-            "* TODO %?\n%i" :prepend t)
-          ("c" "PC"
-            entry (file+headline "~/.org/PC.org" "Tasks")
-            "* TODO %?\n%i" :prepend t)
-          ("h" "Home"
-            entry (file+headline "~/.org/Home.org" "Tasks")
-            "* TODO %?\n%i" :prepend t)))
+        '(("i" "Inbox" entry
+           (file+headline (lambda () (expand-file-name "inbox.org" org-directory)) "Tasks")
+           "* TODO %i%?")
 
+          ("t" "Tickler" entry
+           (file+headline (lambda () (expand-file-name "tickler.org" org-directory)) "Tickler")
+           "* %i%? \n %U")))
+
+  (setq org-agenda-custom-commands
+        '(("w" "At work" tags-todo "@work"
+           ((org-agenda-overriding-header "Work")))
+          ("h" "At home" tags-todo "@home"
+           ((org-agenda-overriding-header "Home")))))
+  
+  (setq org-agenda-files '("~/.org/inbox.org"
+                           "~/.org/tickler.org"))
+
+  (setq org-refile-targets '(("~/.org/inbox.org" :maxlevel . 3)
+                             ("~/.org/someday.org" :level . 1)
+                             ("~/.org/tickler.org" :maxlevel . 2)))
+  
   ;; Keybinds
   (pg/leader
-   "X" #'org-capture)
-
+    "X" #'org-capture)
+  
   (pg/leader
-   :keymaps 'org-mode-map
-   "t l" #'(org-toggle-link-display :wk "link display")
-   "t s" #'(flyspell-mode :wk "spell check"))
+    :keymaps 'org-mode-map
+    "t l" #'(org-toggle-link-display :wk "link display")
+    "t s" #'(flyspell-mode :wk "spell check"))
     
   :general
   (:states 'normal :keymaps 'org-src-mode-map
-   "C-c C-c" #'org-edit-src-exit)
+           "C-c C-c" #'org-edit-src-exit)
   (:states 'normal :keymaps 'org-mode-map
-   "RET" #'org-return))
+           "RET" #'org-return))
    
 (use-package htmlize
   :after org
