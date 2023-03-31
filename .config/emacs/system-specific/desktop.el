f13d5dfdde42d1e5b2879902585afe2b486cc692
Ben Whitley <dev@purplg.com>
2023-03-31 11:01:03 -0400

    start daemon with desktop

--- .config/emacs/system-specific/desktop.el
+++ .config/emacs/system-specific/desktop.el
@@ -8,4 +8,8 @@
 (when (network-lookup-address-info "homeassistant.lan")
   (require 'pg-hass))
 
+(when (daemonp)
+  (pg/load-file "daemon.el"
+                (expand-file-name "system-specific/" pg/config-dir)))
+
 (provide 'pg-desktop)
