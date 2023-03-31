c8011bfd3ca27ba7ec458433ef82fa89e4741554
Ben Whitley <dev@purplg.com>
2023-03-31 11:00:36 -0400

    nix update

--- .config/emacs/modules/pg-nix.el
+++ .config/emacs/modules/pg-nix.el
@@ -1,14 +1,18 @@
 ;;; --- -*- lexical-binding: t; -*-
 
 (use-package nix-mode
+  :hook (nix-mode . eglot-ensure)
   :init
   (with-eval-after-load 'lsp-mode
     (add-to-list 'lsp-language-id-configuration '(nix-mode . "nix"))
     (lsp-register-client
      (make-lsp-client :new-connection (lsp-stdio-connection '("rnix-lsp"))
                       :major-modes '(nix-mode)
-                      :server-id 'nix)))
-  (add-hook 'nix-mode-hook #'lsp)
+                      :server-id 'nix))
+    (add-hook 'nix-mode-hook #'lsp))
+  (with-eval-after-load 'eglot
+    (when (executable-find "nil")
+        (add-to-list 'eglot-server-programs '(nix-mode . ("nil")))))
   (add-hook 'nix-mode-hook #'corfu-mode))
 
 
