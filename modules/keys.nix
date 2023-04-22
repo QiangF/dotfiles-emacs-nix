14728758c322644379000f3ac3179543e06fdb93
Ben Whitley <dev@purplg.com>
2023-04-22 16:45:34 -0400

    rofi keybinds

--- modules/keys.nix
+++ modules/keys.nix
@@ -1,6 +1,12 @@
 { pkgs, ... }: {
   programs.rofi = {
     enable = true;
+    extraConfig = {
+      kb-row-down = "Down,Control+n,Control+j";
+      kb-row-up = "Up,Control+p,Control+k";
+      kb-accept-entry = "Control+m,Return,KP_Enter";
+      kb-remove-to-eol = "";
+    };
     pass.enable = true;
     pass.extraConfig = ''
       help_color="#FF0000"
