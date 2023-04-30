d58b33ea74eafc127d75452ecc50aae5e1c56d64
Ben Whitley <dev@purplg.com>
2023-04-30 08:28:08 -0400

    update env stuff

--- .config/emacs/modules/pg-environment.el
+++ .config/emacs/modules/pg-environment.el
@@ -3,6 +3,7 @@
 (use-package exec-path-from-shell
   :functions exec-path-from-shell-initialize
   :init
+  (setq exec-path-from-shell-variables '("PATH" "MANPATH" "SSH_AUTH_SOCK"))
   (exec-path-from-shell-initialize))
 
 (use-package envrc
@@ -10,6 +11,7 @@
   (envrc-global-mode))
 
 (use-package keychain-environment
+  :if (executable-find "keychain")
   :init
   (keychain-refresh-environment))
 
