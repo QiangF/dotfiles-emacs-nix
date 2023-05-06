17b86f8253e6789cd8c8bac1cb065a060593eb6b
Ben Whitley <dev@purplg.com>
2023-05-06 11:50:52 -0400

    add notes

---
+++ .config/emacs/modules/pg-notes.el
@@ -0,0 +1,22 @@
+;;; --- -*- lexical-binding: t; -*-
+
+(use-package denote
+  :init
+  (setq denote-directory "~/.notes/")
+  (setq denote-prompts '(title keywords))
+  (setq denote-known-keywords '(emacs project linux lan idea))
+
+  (defun pg/denote-today ()
+    "Create a denote entry named after today."
+    (interactive)
+    (denote
+     (format-time-string "%A %e %B %Y")
+     '("journal")))
+
+  (evil-define-key '(normal visual) 'global
+    (kbd "<leader> x") (make-sparse-keymap)
+    (kbd "<leader> x n") #'denote
+    (kbd "<leader> x j") #'pg/denote-today
+    (kbd "<leader> x f") #'denote-open-or-create))
+
+(provide 'pg-notes)
