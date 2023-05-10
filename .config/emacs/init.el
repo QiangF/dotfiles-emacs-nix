c82fa560a27d1b0ed34831ebcd653f75e7d0ad84
Ben Whitley <dev@purplg.com>
2023-05-09 21:18:44 -0400

    just load system stuff in init

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -71,4 +71,10 @@
 (elpaca-wait)
 
 (load-file (file-name-concat pg/config-dir "modules.el"))
-(load-file (file-name-concat pg/config-dir "system.el"))
+
+;; Try to load system-specific modules (hostname)
+(pg/try-load-file (file-name-concat "system/"
+                                    (file-name-with-extension (system-name) ".el")))
+
+;; local.el is an untracked / .gitignored file. Load it if it exists
+(pg/try-load-file "local.el")
--- .config/emacs/system.el
+++
@@ -1,9 +0,0 @@
-;;; --- -*- lexical-binding: t; -*-
-(require 'pg-aliases)
-
-;; Try to load system-specific modules (hostname)
-(pg/try-load-file (file-name-concat "system/"
-                                    (file-name-with-extension (system-name) ".el")))
-
-;; local.el is an untracked / .gitignored file. Load it if it exists
-(pg/try-load-file "local.el")
