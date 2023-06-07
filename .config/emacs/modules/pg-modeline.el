c519e2ccd8e0c11282eba126d732e69178f25bd1
Ben Whitley <dev@purplg.com>
2023-06-06 20:30:04 -0400

    switch to mood-line

--- .config/emacs/modules/pg-modeline.el
+++ .config/emacs/modules/pg-modeline.el
@@ -1,7 +1,17 @@
 ;;; --- -*- lexical-binding: t; -*-
 
 (use-package doom-modeline
+  :disabled
   :init
   (doom-modeline-mode 1))
+
+(use-package mood-line
+  :config
+  (setopt mood-line-show-cursor-point t)
+  (setopt mood-line-show-encoding-information t)
+  (setopt mood-line-show-eol-style t)
+  (setopt mood-line-show-indentation-style t)
+  (setopt mood-line-show-major-mode t)
+  (mood-line-mode))
 
 (provide 'pg-modeline)
