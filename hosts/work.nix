55ce19be850222960030af9fc406ee48fc78fa42
Ben Whitley <dev@purplg.com>
2023-07-19 19:08:06 -0400

    remove firefox and wayland from work

--- hosts/work.nix
+++ hosts/work.nix
@@ -2,12 +2,10 @@
 {
   imports = [
     ../modules/dev.nix
-    ../modules/firefox.nix
     ../modules/keys.nix
     ../modules/shell.nix
     ../modules/terminal.nix
     ../modules/tools.nix
-    ../modules/wayland.nix
   ];
 
   home = {
