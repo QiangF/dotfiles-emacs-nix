f2154bd0190406c971fe0c2628d1eaf379a1a51d
Ben Whitley <dev@purplg.com>
2023-08-06 17:38:19 -0400

    jinx keybind

--- .config/emacs/modules/pg-spellcheck.el
+++ .config/emacs/modules/pg-spellcheck.el
@@ -5,6 +5,9 @@
 (use-package jinx
   :hook
   (org-mode . jinx-mode)
-  (markdown-mode . jinx-mode))
+  (markdown-mode . jinx-mode)
+  :config
+  (evil-define-key* 'normal jinx-mode-map
+    (kbd "<leader> SPC") #'("spelling" . jinx-correct)))
 
 (provide 'pg-spellcheck)
